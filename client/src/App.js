27c4f8cbbbcb3a1f67d3ddb3b7eb1a54bf24cfcd
itsmehujo <itsmehujo@gmail.com>
2022-05-08 13:08:19 +0200

    no boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to hujo.
-        </p>
-        <a
-          className="App-link"
-          href="/auth/google"
-        >
-          Sign in with Google
-        </a>
-        <a
-          className="App-link"
-          href="/api/current_user"
-        >
-          Check user
-        </a>
-        <a
-          className="App-link"
-          href="/api/logout"
-        >
-          Logout
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(<>
+    Hi there
+  </>)
 }
 
 export default App;
